dc3ff7804881a0a548fc32295bd8b6191e18d952
auris48 <auris48@gmail.com>
2022-07-26 18:47:33 +0100

    Added equals haschode and tostring methods to item

--- src/main/java/com/qa/ims/persistence/domain/Item.java
+++ src/main/java/com/qa/ims/persistence/domain/Item.java
@@ -58,6 +58,44 @@ public class Item {
         this.itemPrice = itemPrice;
     }
 
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return Double.compare(item.itemPrice, itemPrice) == 0 && id.equals(item.id) && itemName.equals(item.itemName) && itemStockDate.equals(item.itemStockDate) && itemDescription.equals(item.itemDescription);
+    }
+
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        Double itemPrice = this.itemPrice;
+        result = prime * result + ((itemName == null) ? 0 : itemName.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((itemDescription == null) ? 0 : itemDescription.hashCode());
+        result = prime * result + ((itemStockDate == null) ? 0 : itemStockDate.hashCode());
+        result = prime * result + ((itemPrice == null) ? 0 : itemPrice.hashCode());
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return " id: " + id +
+                " item name: "  + itemName  +
+                " item stock date:  " + itemStockDate +
+                " item description: " + itemDescription +
+                " item price: " + itemPrice;
+    }
 
 
 }
