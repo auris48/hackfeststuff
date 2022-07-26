ef87576fb8c61c9f33f580c2380844c57f931d16
auris48 <auris48@gmail.com>
2022-07-26 18:20:31 +0100

    Added Item class, craeted constructor

---
+++ src/main/java/com/qa/ims/persistence/domain/Item.java
@@ -0,0 +1,21 @@
+package com.qa.ims.persistence.domain;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Item {
+
+    private Long id;
+    private String itemName;
+    private LocalDate itemStockDate;
+    private String itemDescription;
+    private double itemPrice;
+
+    public Item(String itemName, LocalDate itemStockDate, String itemDescription, double itemPrice) {
+        this.itemName = itemName;
+        this.itemStockDate = itemStockDate;
+        this.itemDescription = itemDescription;
+        this.itemPrice = itemPrice;
+    }
+
+}
