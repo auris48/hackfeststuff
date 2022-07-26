4d2a6166f9b1ba810ee9689f483de7cc9003fb71
auris48 <auris48@gmail.com>
2022-07-26 19:01:14 +0100

    Added item controller class

---
+++ src/main/java/com/qa/ims/controller/ItemController.java
@@ -0,0 +1,43 @@
+package com.qa.ims.controller;
+
+import com.qa.ims.persistence.dao.ItemDAO;
+import com.qa.ims.persistence.domain.Item;
+import com.qa.ims.utils.Utils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ * Takes in Item details for CRUD functionality
+ *
+ */
+public class ItemController implements CrudController<Item> {
+
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	private ItemDAO itemDao;
+	private Utils utils;
+
+
+	@Override
+	public List<Item> readAll() {
+		return null;
+	}
+
+	@Override
+	public Item create() {
+		return null;
+	}
+
+	@Override
+	public Item update() {
+		return null;
+	}
+
+	@Override
+	public int delete() {
+		return 0;
+	}
+}
